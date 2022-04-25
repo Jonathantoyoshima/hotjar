b4708688ef9e16bc71f5efeb64f8e26cd156b4c5
Jonathan Satoru Toyoshima <jonathan.toyoshima@lojasrenner.com.br>
2022-04-25 17:07:27 -0300

    add styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
+
+const StyledText = styled.p`
+  color: red;
+  font-size: 2em;
+  border: 1px solid red;
+  padding: 0.5em;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <StyledText>Here a styled Text</StyledText>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
